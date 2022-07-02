e5ba61e1c89927bfbb0d1838b1f0ea6a4cb3ebd8
Leo Eiji <leoeiji1234@gmail.com>
2022-07-02 09:40:54 -0300

    Changed Appbar from index page

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,22 +1,44 @@
 <template>
   <v-app dark>
-    <v-app-bar
-      v-if="$auth.loggedIn"
-      app
-      dense
-      clipped-left
-      color="primary"
-      flat
-    >
-      <v-toolbar-title class="text-subtitle-1 grey--text text--lighten-2">
-        Bem-vindo,
-        <strong>{{ $auth.user.first_name }} {{ $auth.user.last_name }}</strong
-        >!
-      </v-toolbar-title>
+    <v-app-bar v-if="$auth.loggedIn" app clipped-left color="primary" flat>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title class="grey--text text--lighten-2">
+            {{ $auth.user.username }}
+          </v-list-item-title>
+          <v-list-item-subtitle
+            class="grey--text text--lighten-2 d-flex align-center"
+          >
+            {{ $auth.user.email }}
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-icon
+                  v-on="on"
+                  v-bind="attrs"
+                  class="ml-1 text--lighten-2"
+                  :class="false ? 'green--text' : 'red--text'"
+                  x-small
+                >
+                  {{ false ? 'fa-circle-check' : 'fa-circle-exclamation' }}
+                </v-icon>
+              </template>
+              <span>
+                {{ false ? 'E-mail verificado!' : 'E-mail não verificado!' }}
+              </span>
+            </v-tooltip>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
       <v-spacer></v-spacer>
-      <v-btn text small class="grey--text text--lighten-2" @click="logout"
-        >Logout</v-btn
+      <v-btn
+        text
+        small
+        class="red--text font-weight-bold"
+        @click="$auth.logout('local')"
       >
+        Sair
+      </v-btn>
     </v-app-bar>
     <v-main>
       <Nuxt />
@@ -35,9 +57,7 @@ export default {
   },
 
   methods: {
-    logout() {
-      this.$auth.logout('local')
-    },
+    logout() {},
   },
 }
 </script>
